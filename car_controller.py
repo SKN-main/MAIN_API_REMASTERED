a003a6ad15f24495d5f07aa717375e5ba4a0b214
Krystian Juszczyk <juszczyk-krystian@wp.pl>
2024-05-23 18:57:14 +0200

    New format added.

--- car_controller.py
+++ car_controller.py
@@ -1,21 +1,18 @@
 import sys
 import threading
-import json
-import time
 
-MAX_SPEED = 1
-MIN_SPEED = -1
+MAX_SPEED = 100
+MIN_SPEED = -100
+
 
 class Controller:
     def __init__(self, output=sys.stdout):
         self._current_turn = 0
         self._current_speed = 0
         self._output = output
-
-
         self.turn = 0
         self.speed = 0
-        self.sensor_data = [0, 0, 0]
+        self.sensor_data = [0, 0, 0, 0, 0, 0]  # car has 6 sensors
 
         """This code creates a new thread that will run the self.read method in the background.
         The main program will continue execution without waiting for the self.read method to finish.
@@ -32,7 +29,6 @@ class Controller:
         """Run self test. Will move the wheels at full speed!"""
         self.send('selftest\n')
 
-    
     @property
     def turn(self):
         return self._current_turn
@@ -50,12 +46,7 @@ class Controller:
 
         self._current_turn = min(max(angle, -max_angle), max_angle)
 
-        json_message = json.dumps({
-            'turn_direction': turn_direction,
-            'turn_level': self._current_turn
-            }
-        )
-        self.send(json_message)
+        self.send()
 
     @property
     def speed(self):
@@ -66,69 +57,33 @@ class Controller:
         """Set the speed of both wheels."""
         if value == self._current_speed:
             return
-        if value == 0:
-            self.stop()
-            return
-        move_direction = 'F' if value > 0 else 'R'
-    
-        speed_level = int(abs(value))
-        speed_level = min(speed_level, MAX_SPEED)
-
         # Constraint the speed to the [-max_speed; max_speed] range
         self._current_speed = min(max(value, -MAX_SPEED), MAX_SPEED)
+        self.send()
 
-        json_message = json.dumps({
-            'move_direction': move_direction,
-            'speed_level': self._current_speed
-            }
-        )
-        self.send(json_message)
-
-    def stop(self):
-        """Stop the car."""
-        self._current_speed = 0
-
-        json_message = json.dumps({
-            'direction': 'S',
-            'speed_level': 0
-            }
-        )
-        self.send(json_message)
-
-    def send(self, packet):
+    def send(self, packet=''):
         """Send a packet to the controller directly. Low-level."""
-        print(f'Sent: ')
+        if packet == '':
+            packet = (f'{self._current_speed};{self._current_turn};'
+                      f'{self.sensor_data[0]};{self.sensor_data[1]};'
+                      f'{self.sensor_data[2]};{self.sensor_data[3]};'
+                      f'{self.sensor_data[4]};{self.sensor_data[5]}\n')
+
         self._output.write(packet)
-        print(f'\n')
 
     def read(self):
         """Read the serial port and parse incoming packets."""
         while True:
-            # example data
-            time.sleep(1)
-            data = {
-                'speed': controller.speed,
-                'turn': controller.turn,
-                'distance_sensors': [3, 4, 5],
-                'accelerometer': 123
-            }
-            if data:
-                speed = data['speed']
-                turn = data['turn']
-                distance_sensors = data['distance_sensors']
-                accelerometer = data['accelerometer']
-                print(f'speed: {speed}, turn: {turn}, '
-                    f'distance_sensors[0]: {distance_sensors[0]}, '
-                    f'distance_sensors[1]: {distance_sensors[1]}, '
-                    f'distance_sensors[2]: {distance_sensors[2]}, '
-                    f'accelerometer: {accelerometer}\n')
+            pass
 
     def __repr__(self):
-        return f'Controller(turn={self.turn}, speed={self.speed})'
+        return f'Car(turn={self.turn}, speed={self.speed})'
 
 
 if __name__ == '__main__':
     controller = Controller()
+
     controller.turn = 21
     controller.speed = -0.1
     controller.speed = 0.2
+    print(controller.__repr__())
